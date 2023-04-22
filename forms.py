3c56d04e7df1f015aba58dd6e0bb730b6113bca6
Ruturaj Jadhav <mail.jadhavrj2121@gmail.com>
2023-04-22 20:06:02 +0530

    Session 14

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Email, Length, EqualTo, DataRequired
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, FileField, TextAreaField, URLField, SelectField, FloatField
+from wtforms.validators import Email, Length, EqualTo, DataRequired, URL, NumberRange
 
 class AdminRegisterationForms(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
@@ -13,3 +13,14 @@ class AdminLoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired(),Length(min=6)])
     submit = SubmitField("Login")
+
+class AddBookForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    author = StringField("Author", validators=[DataRequired()])
+    isbn = StringField("ISBN",validators=[DataRequired()])
+    genre = SelectField("Genre",validators=[DataRequired(),], choices = [('ed','Educational'), ('ck','Cooking'), ('fc','Fiction')])
+    shoplink = URLField("Shop Link",validators=[DataRequired(), URL()])
+    rating = FloatField("Rating", validators=[DataRequired(), NumberRange(min=0, max=10)])
+    coverimage = FileField("Upload Cover Image", validators=[DataRequired()])
+    summary = TextAreaField("Tiny Summary",render_kw={"placeholder":"few words ..."}, validators=[DataRequired()])
+    save =SubmitField("Save")
--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 from flask import Flask, render_template
-from forms import AdminRegisterationForms, AdminLoginForm
+from forms import AdminRegisterationForms, AdminLoginForm, AddBookForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secret_key_33'
-# msg = "Hello, Ruturaj"
-# website_name = "Readaholic"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+
 data = {
     "website_name": "Readaholic",
     "Author" : "Ruturaj Jadhav"
 }
+
+db= SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(32), nullable=False)
+
+    def __repr__(self):
+        return f"User(email:{self.email})"
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(120), nullable= False)
+    author = db.Column(db.String(120), nullable= False)
+    isbn = db.Column(db.Integer, unique= True, nullable= False)
+    genre = db.Column(db.String(60), nullable= False)
+    shoplink = db.Column(db.String(120), nullable= True)
+    rating = db.Column(db.Float, nullable= False)
+    cover_image = db.Column(db.String(120), nullable= True, default="default.jpg")
+    summary = db.Column(db.Text, nullable= False)
+
+    def __repr__(self):
+        return f"Book(title: {self.title}, isbn: {self.isbn})"
+
 
 @app.route("/")
 def home():
@@ -25,13 +54,25 @@ def about():
 def register():
     form = AdminRegisterationForms()
     if form.validate_on_submit():
-        print(form.data)
+        _email = form.data['email']
+        _password = form.data['password']
+        user = User(email= _email, password= _password)
+        db.session.add(user)
+        db.session.commit()
+        print("User added")
     return render_template("register.html", form=form)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form= AdminLoginForm()
     return render_template("login.html", form =form)
+
+@app.route("/add_books", methods=["GET", "POST"])
+def add_book():
+    form= AddBookForm()
+    if form.validate_on_submit():
+        print(form.data)
+    return render_template("add_book.html", form=form)
 
     
 # @app.route("/name1")
